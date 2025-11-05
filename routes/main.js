ed554012f6ce7dd6c5df688dd392372a33b2cdaf
Chris <148207385+Xencil@users.noreply.github.com>
2025-11-05 16:10:04 +0000

    version before extension

---
+++ routes/main.js
@@ -0,0 +1,38 @@
+// Create a new router
+const express = require("express");
+const router = express.Router();
+var shopData = {shopName: "The Thirsty Student", 
+                productCategories:["Beer", "Wine", "Soft Drinks", "Hot Drinks"]}
+
+
+// Handle the main routes
+
+router.get('/',function(req,res){
+    res.render('index.ejs', shopData)
+});
+
+router.get("/about", (req, res) => {
+    res.render("about.ejs", shopData)
+}); 
+
+router.get("/search", (req, res) => {
+    res.render("search.ejs", shopData)
+}); 
+
+router.get('/search_result', function (req, res) {
+    // TODO: search in the database
+    res.send("You searched for " + req.query.search_text + " in " + req.query.category);
+});
+
+router.get("/register", (req,res) => {
+    res.render("register.ejs",  shopData); 
+}); 
+
+router.post("/registered", (req,res) => { 
+    res.send(' Hello '+ req.body.first + ' '+ req.body.last +' you are now registered! We will send a confirmation email to '+ req.body.email );   
+})
+
+
+
+// Export the router object so index.js can access it
+module.exports = router;
